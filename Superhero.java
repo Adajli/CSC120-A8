a3ae8857fb8abc5cc74eba25f875500b3f49865f
Adajli <ajli@smith.edu>
2024-11-07 14:09:31 -0500

    Modified the chunk for use and fly methods and inserted statement for rest() function.

--- Superhero.java
+++ Superhero.java
@@ -38,7 +38,7 @@ public class Superhero implements Contract {
      * @param item
      */
     public void examine(String item) {
-        if(item == null){
+        if (item == null) {
             throw new RuntimeException("Can't examine that item.");
         }
         System.out.println("Examining the item.");
@@ -51,11 +51,11 @@ public class Superhero implements Contract {
      * @param item
      */
     public void use(String item) {
-        if(item == null){
+        if (item == null) {
             throw new RuntimeException("Can't use that item.");
         }
         System.out.println("Using the item.");
-        System.out.println(item +" is used.");
+        System.out.println(item + " is used.");
     }
 
     /**
@@ -80,7 +80,7 @@ public class Superhero implements Contract {
         boolean fly = false;
         if (x >= 0 && y > 0) {
             fly = true;
-            System.out.println(name + "is flying for "+ x +" units long and " + y + "units high.");
+            System.out.println(name + "is flying for " + x + " units long and " + y + "units high.");
         } else {
             throw new RuntimeException(name + "can't fly at the distance.");
         }
@@ -111,7 +111,7 @@ public class Superhero implements Contract {
      * Rest method
      */
     public void rest() {
-
+        System.out.println(name + " is resting.");
     }
 
     /**
